9e0f886289bf7d628450e33e692b4250b9a7c39f
DiabateJS <diabatejs@yahoo.fr>
2021-12-29 17:03:42 +0100

    Décomposition de l'interface en composants angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopbarComponent,
+    DashbordComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
